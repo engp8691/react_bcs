e847decb6a36a95381a45f030b012827f6d8f506
root <root@rtc.netbriefings.com>
2018-07-08 11:07:33 -0400

    Message

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import Course from './Course.js';
 class App extends Component {
 	state = ({
 		courses: [
-			{id: '1000001', name: 'Chinese Level 1', teacher: 'Wang Lan', time: '2:00 - 3:30pm', room: '104', fallprice: '220', springprice: '220', selected: true, termregistered: {fallselected: false, springselected: false}},
-			{id: '1000003', name: 'Chinese Level 2', teacher: 'Peng Li', time: '2:00 - 3:30pm', room: '114', fallprice: '220', springprice: '220', selected: false, termregistered: {fallselected: false, springselected: false}},
-			{id: '1000005', name: 'Chinese Level 3', teacher: 'Li Qingwei', time: '2:00 - 3:30pm', room: '124', fallprice: '220', springprice: '220', selected: false, termregistered: {fallselected: false, springselected: false}},
-			{id: '1000007', name: 'Chinese Level 4', teacher: 'Li Lijia', time: '2:00 - 3:30pm', room: '134', fallprice: '220', springprice: '220', selected: true, termregistered: {fallselected: true, springselected: true}}
+			{id: '1000001', name: 'Chinese Level 1', teacher: 'Wang Lan', biofile_en: 'wanglan_en.html', biofile_cn: 'wanglan_cn.html', time: '2:00 - 3:30pm', room: '104', fallprice: '220', springprice: '220', selected: true, termregistered: {fallselected: false, springselected: false}},
+			{id: '1000003', name: 'Chinese Level 2', teacher: 'Peng Li', biofile_en: 'pengli_en.html', biofile_cn: 'pengli_cn.html', time: '2:00 - 3:30pm', room: '114', fallprice: '220', springprice: '220', selected: false, termregistered: {fallselected: false, springselected: false}},
+			{id: '1000005', name: 'Chinese Level 3', teacher: 'Li Qingwei', biofile_en: 'liqingwei_en.html', biofile_cn: 'liqingwei_cn.html', time: '2:00 - 3:30pm', room: '124', fallprice: '220', springprice: '220', selected: false, termregistered: {fallselected: false, springselected: false}},
+			{id: '1000007', name: 'Chinese Level 4', teacher: 'Li Lijia', biofile_en: 'lilijia_en.html', biofile_cn: 'lilijia_cn.html', time: '2:00 - 3:30pm', room: '134', fallprice: '220', springprice: '220', selected: true, termregistered: {fallselected: true, springselected: true}}
 		]
 	});
 
@@ -51,6 +51,18 @@ class App extends Component {
 		this.setState( { courses: courses } );
 	}
 
+	openTeacherIntroduction = (event, id) => {
+		const courseIndex = this.state.courses.findIndex( c => {
+			return c.id === id;
+		});
+
+		const course = {
+			...this.state.courses[courseIndex]
+		};
+
+		console.log("open file", course.biofile_en);
+	}
+
 	render() {
 		let courses = null;
 		courses = (this.state.courses.map((course, index) => {
@@ -68,8 +80,10 @@ class App extends Component {
 					fallselected={course.termregistered.fallselected}
 					springselected={course.termregistered.springselected}
 					termchanged={(event) => this.termChangedHandler(event, course.id)}
-					changed={(event) => this.selectionChangedHandler(event, course.id )} />
-				);
+					changed={(event) => this.selectionChangedHandler(event, course.id )}
+					openTeacher={(event) => this.openTeacherIntroduction(event, course.id)}
+				/>
+			);
 		}));
 
 		console.log(62, courses);
--- src/Course.js
+++ src/Course.js
@@ -1,40 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Course.css';
 
-const course = ( props ) => {
-	return(
+class Course extends Component{
+	render(){
+		return (
 		<div className={classes.Course}>
-			<input type="checkbox" onChange={props.changed} value={props.id} checked={props.selected} /> <span>{props.name}</span>
+			<input type="checkbox" onChange={this.props.changed} value={this.props.id} />
+			<span>{this.props.name}</span>
 			<table border="0px"  className={classes.info}>
 				<tbody>
 				<tr>
 					<td width="40px"></td>
 					<td className={classes.info_teacher}>Teacher:</td>
-					<td className={classes.info_teacher_name}>{props.teacher}</td>
+					<td className={classes.info_teacher_name}><button type="button" onClick={this.props.openTeacher} className={classes.teacherButton}>{this.props.teacher}</button></td>
 				</tr>
 				<tr>
 					<td width="40px"></td>
 					<td className={classes.info_teacher}>Room:</td>
-					<td className={classes.info_teacher_name}>{props.room}</td>
+					<td className={classes.info_teacher_name}>{this.props.room}</td>
 				</tr>
 				<tr>
 					<td width="40px"></td>
 					<td className={classes.info_teacher}>Time:</td>
-					<td className={classes.info_teacher_name}>{props.time}</td>
+					<td className={classes.info_teacher_name}>{this.props.time}</td>
 				</tr>
 				<tr>
 					<td width="40px"></td>
-					<td className={classes.info_teacher} colSpan="2"><input type="checkbox" value="fall" checked={props.fallselected} onChange={props.termchanged} /> ${props.fallprice} Fall Term</td>
+					<td className={classes.info_teacher} colSpan="2"><input type="checkbox" value="fall" checked={this.props.fallselected} onChange={this.props.termchanged} /> ${this.props.fallprice} Fall Term</td>
 				</tr>
 				<tr>
 					<td width="40px"></td>
-					<td className={classes.info_teacher} colSpan="2"><input type="checkbox" value="spring" checked={props.springselected} onChange={props.termchanged} /> ${props.springprice} Spring Term</td>
+					<td className={classes.info_teacher} colSpan="2"><input type="checkbox" value="spring" checked={this.props.springselected} onChange={this.props.termchanged} /> ${this.props.springprice} Spring Term</td>
 				</tr>
 				</tbody>
 			</table>
 		</div>
-	);
+		);
+	}
 }
 
-export default course;
+export default Course;
 
